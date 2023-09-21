becb7a9f995bb73fc55d51791ed60f47112538fd
heiung2001 <hieunc.hust@gmail.com>
2023-09-21 10:10:14 +0700

    minor changes to adapt with kaggle platform

--- src/models/baseline/utils/make_vocabs_for_questions_answers.py
+++ src/models/baseline/utils/make_vocabs_for_questions_answers.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 import numpy as np
+import argparse
 from collections import defaultdict
 
 
@@ -59,3 +60,20 @@ def make_vocab_answers(input_dir, n_answers):
     print('Make vocabulary for answers')
     print('The number of total words of answers: %d' % len(answers))
     print('Keep top %d answers into vocab' % n_answers)
+
+
+def main(args):
+    input_dir = args.input_dir
+    n_answers = args.n_answers
+    make_vocab_questions(input_dir + '/Questions')
+    make_vocab_answers(input_dir + '/Annotations', n_answers)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default='/run/media/hoosiki/WareHouse3/mtb/datasets/VQA',
+                        help='directory for input questions and answers')
+    parser.add_argument('--n_answers', type=int, default=1000,
+                        help='the number of answers to be kept in vocab')
+    args = parser.parse_args()
+    main(args)
